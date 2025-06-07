c57443cc9177134ba8166ff02b416b83e61fd308
Charles Giessen <charles@lunarg.com>
2025-06-06 20:07:12 -0500

    apidump: Sort generated code
    The old api dump code generator based on OutputGenerator did not sort
    the generated code. By default VulkanObject does run the sort procedures
    which made comparing the new and old codegen difficult. Thus, the
    sorting was disabled for the codegen rewrite. This commit enables the
    sorting, which fixes some errors that were introduced by the lack of
    sorting (while using VulkanObject). While this commit contains a massive
    change list, it is merely moving code around.

--- scripts/generate_source.py
+++ scripts/generate_source.py
@@ -154,8 +154,8 @@ def RunGenerators(api: str, registry: str, video_registry: str, directory: str, 
             SetMergedApiNames(None)
 
         baseOptions = BaseGeneratorOptions(customFileName = target)
-        baseOptions.sortProcedure = None
-        baseOptions.alignFuncParam = 0
+        # baseOptions.sortProcedure = None
+        # baseOptions.alignFuncParam = 0
 
         # Create the registry object with the specified generator and generator
         # options. The options are set before XML loading as they may affect it.
